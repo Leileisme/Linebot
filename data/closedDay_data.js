56ee8752a5e254bc6e48d9d171cc5bcdf4b76536
Leileisme <onlymymind1108@gmail.com>
2024-01-04 10:55:14 +0800

    修「closedDay_data」的bug

--- data/closedDay_data.js
+++ data/closedDay_data.js
@@ -9,18 +9,33 @@ export const closedDayUpload = async (e) => {
     const now = new Date()
     const nowYear = now.getFullYear()
     const nowMonth = now.getMonth() + 1
+    // console.log(nowYear, nowMonth)
 
     for (let i = nowMonth; i <= 24; i++) {
       const template = closedDayTemplates()
-      if (i < 13) {
-        template.body.contents[0].url = `https://raw.githubusercontent.com/Leileisme/Linebot/main/images/date/date_${nowYear}_${i}.jpg`
-
-        closedDayData.push(template)
-      } else {
-        template.body.contents[0].url = `https://raw.githubusercontent.com/Leileisme/Linebot/main/images/date/date_${
-          nowYear + 1
-        }_0${i - 12}.jpg`
-        closedDayData.push(template)
+      if (i <= 12) {
+        if (i >= 10) {
+          template.body.contents[0].url = `https://raw.githubusercontent.com/Leileisme/Linebot/main/images/date/date_${nowYear}_${i}.jpg`
+          closedDayData.push(template)
+        } else {
+          template.body.contents[0].url = `https://raw.githubusercontent.com/Leileisme/Linebot/main/images/date/date_${nowYear}_0${i}.jpg`
+          closedDayData.push(template)
+        }
+        console.log(template.body.contents[0].url)
+      } else if (i > 12) {
+        const j = i - 12
+        if (j >= 10) {
+          template.body.contents[0].url = `https://raw.githubusercontent.com/Leileisme/Linebot/main/images/date/date_${
+            nowYear + 1
+          }_${j}.jpg`
+          closedDayData.push(template)
+        } else if (j < 10) {
+          template.body.contents[0].url = `https://raw.githubusercontent.com/Leileisme/Linebot/main/images/date/date_${
+            nowYear + 1
+          }_0${j}.jpg`
+          closedDayData.push(template)
+        }
+        // console.log(template.body.contents[0].url)
       }
     }
 
